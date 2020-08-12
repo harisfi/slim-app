b1eb22fcb06eeb0ac37430de3e782de9aa84317e
Haris Firmansyah <acc.cp.haris@gmail.com>
2020-08-12 22:00:54 +0700

    Added new routes

--- src/routes.php
+++ src/routes.php
@@ -21,4 +21,83 @@ return function (App $app) {
         // show message
         echo "it's an about page!";
     });
+
+    $app->get("/coffees/", function (Request $request, Response $response){
+        $sql = "SELECT * FROM coffees";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+        return $response->withJson(["status" => "success", "data" => $result], 200);
+    });
+
+    $app->get("/coffees/{id}", function (Request $request, Response $response, $args){
+        $id = $args["id"];
+        $sql = "SELECT * FROM coffees WHERE coffee_id=:id";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute([":id" => $id]);
+        $result = $stmt->fetch();
+        return $response->withJson(["status" => "success", "data" => $result], 200);
+    });
+
+    $app->get("/coffees/search/", function (Request $request, Response $response, $args){
+        $keyword = $request->getQueryParam("keyword");
+        $sql = "SELECT * FROM coffees WHERE name LIKE '%$keyword%' OR desc LIKE '%$keyword%' OR source LIKE '%$keyword%'";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+        return $response->withJson(["status" => "success", "data" => $result], 200);
+    });
+
+    $app->post("/coffees/", function (Request $request, Response $response){
+
+        $new_book = $request->getParsedBody();
+    
+        $sql = "INSERT INTO coffees (name, source, desc) VALUE (:name, :source, :desc)";
+        $stmt = $this->db->prepare($sql);
+    
+        $data = [
+            ":name" => $new_book["name"],
+            ":source" => $new_book["source"],
+            ":desc" => $new_book["desc"]
+        ];
+    
+        if($stmt->execute($data))
+           return $response->withJson(["status" => "success", "data" => "1"], 200);
+        
+        return $response->withJson(["status" => "failed", "data" => "0"], 200);
+    });
+
+    $app->put("/coffees/{id}", function (Request $request, Response $response, $args){
+        $id = $args["id"];
+        $new_book = $request->getParsedBody();
+        $sql = "UPDATE coffees SET name=:name, source=:source, desc=:desc WHERE coffee_id=:id";
+        $stmt = $this->db->prepare($sql);
+        
+        $data = [
+            ":id" => $id,
+            ":name" => $new_book["name"],
+            ":source" => $new_book["source"],
+            ":desc" => $new_book["desc"]
+        ];
+    
+        if($stmt->execute($data))
+           return $response->withJson(["status" => "success", "data" => "1"], 200);
+        
+        return $response->withJson(["status" => "failed", "data" => "0"], 200);
+    });
+
+    $app->delete("/coffee/{id}", function (Request $request, Response $response, $args){
+        $id = $args["id"];
+        $sql = "DELETE FROM coffee WHERE coffee_id=:id";
+        $stmt = $this->db->prepare($sql);
+        
+        $data = [
+            ":id" => $id
+        ];
+    
+        if($stmt->execute($data))
+           return $response->withJson(["status" => "success", "data" => "1"], 200);
+        
+        return $response->withJson(["status" => "failed", "data" => "0"], 200);
+    });
 };
